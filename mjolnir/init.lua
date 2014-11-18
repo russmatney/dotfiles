0ef8d16a81b05d21035c5bce250c0c577e02463e
Russell Matney <russell.matney@gmail.com>
2014-11-17 23:52:20 -0500

    mjolnir for fast app switch/window resize stolen from @ajoslin

--- mjolnir/init.lua
+++ mjolnir/init.lua
@@ -3,10 +3,37 @@ local hotkey = require "mjolnir.hotkey"
 local window = require "mjolnir.window"
 local fnutils = require "mjolnir.fnutils"
 
-hotkey.bind({"cmd", "alt", "ctrl"}, "D", function()
-  local win = window.focusedwindow()
-  local f = win:frame()
-  f.x = f.x + 10
-  win:setframe(f)
+local modalKey = {"ctrl"}
+
+local resizeMappings = {
+  h={x=0, y=0, w=0.5, h=1},
+  j={x=0, y=0.5, w=1, h=0.5},
+  k={x=0, y=0, w=1, h=0.5},
+  l={x=0.5, y=0, w=0.5, h=1},
+  m={x=0, y=0, w=1, h=1}
+}
+
+for key in pairs(resizeMappings) do
+  hotkey.bind(modalKey, key, function()
+    local win = window.focusedwindow()
+    if win then win:movetounit(resizeMappings[key]) end
+  end)
+end
+
+hotkey.bind(modalKey, "z", function()
+  mjolnir.reload()
 end)
 
+local focusKeys = {
+  c='Google Chrome',
+  t='iTerm',
+  p='Spotify',
+  s='iOS Simulator',
+  m='Messages',
+  x='Xcode'
+}
+for key in pairs(focusKeys) do
+  hotkey.bind(modalKey, key, function()
+    application.launchorfocus(focusKeys[key])
+  end)
+end
