9aabebcb1496d207100ea512af5c2809b1d539fb
Russell Matney <russell.matney@gmail.com>
2015-07-10 23:08:37 -0400

    mjolnir settings

---
+++ mjolnir/init.lua
@@ -0,0 +1,33 @@
+local application = require "mjolnir.application"
+local hotkey = require "mjolnir.hotkey"
+local window = require "mjolnir.window"
+local fnutils = require "mjolnir.fnutils"
+
+local modalKey = {"alt"}
+
+local resizeMappings = {
+  f={x=0, y=0, w=1, h=1}
+}
+
+for key in pairs(resizeMappings) do
+  hotkey.bind(modalKey, key, function()
+    local win = window.focusedwindow()
+    if win then win:movetounit(resizeMappings[key]) end
+  end)
+end
+
+local focusKeys = {
+  c='Google Chrome',
+  e='System Preferences',
+  p='Spotify',
+  i='iTunes',
+  t='iTerm',
+  r='LimeChat',
+  m='Messages',
+  n='nvALT'
+}
+for key in pairs(focusKeys) do
+  hotkey.bind(modalKey, key, function()
+    application.launchorfocus(focusKeys[key])
+  end)
+end
