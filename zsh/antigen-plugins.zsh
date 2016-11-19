afdfe393dae57cf8f9a8f2f4620e9ba86563a0fd
Russell Matney <russ@Russells-MacBook-Pro.local>
2016-11-19 10:46:59 -0500

    fonts, iterm2 color submodules

--- zsh/antigen-plugins.zsh
+++ zsh/antigen-plugins.zsh
@@ -4,13 +4,12 @@ antigen use oh-my-zsh
 
 antigen theme https://gist.github.com/rschmukler/7d9317cde82c58be965d rs2.zsh-theme
 
-antigen bundle zsh-users/zsh-syntax-highlighting 
+antigen bundle zsh-users/zsh-syntax-highlighting
 
 antigen bundle Tarrasch/zsh-autoenv
 antigen bundle yerinle/zsh-gvm
 
 antigen bundle brew
-antigen bundle brew-cask
 antigen bundle osx
 antigen bundle nvm
 antigen bundle rvm
