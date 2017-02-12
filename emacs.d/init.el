2d81db79d405f1285e9c1989b52ffcd2235ef68a
Russell Matney <russell.matney@gmail.com>
2017-02-12 14:58:57 -0500

    fix(emacs): org settings before open first buffer

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,62 @@
 
+(setq org-todo-keywords
+       '((sequence "TODO"
+                   "WORKFLOW" "FIX" "FANCY" "RESEARCH"
+                   "IDEA" "MOVE" "INFO" "BLOG" "POST" "JACK"
+                   "OPENSOURCE"
+                   "STORYX"
+                   "PROTOTYPE"
+                   "TRIAGE"
+                   "|"               ;; <------- more likely to be typed
+                   "TODOLOL"         ;; at the center
+                   "INACTIVE"
+                   "REMINDER"
+                   "SCHEDULED"
+                   "BLOCKED"         ;; closer to 0 == quicker scrolling
+                   "DONE"            ;; <------- closer for Shift-<left> wrapping
+        ))
+)
+
+(setq org-src-fontify-natively t)
+
+(defun my/tangle-dotfiles ()
+  "If the current file is in '~/.dotfiles', the code blocks are tangled"
+  (when (equal (file-name-directory (directory-file-name buffer-file-name))
+               (concat (getenv "HOME") "/dotfiles/emacs.d/"))
+    (org-babel-tangle)
+    (message "%s tangled" buffer-file-name)))
+
+(add-hook 'after-save-hook #'my/tangle-dotfiles)
+
+(setq org-directory "~/Dropbox/todo/")
+(setq org-mobile-inbox-for-pull "~/Dropbox/todo/inbox.org")
+(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+(setq org-mobile-files '("~/Dropbox/todo"))
+
+;;(add-hook 'after-init-hook 'org-mobile-pull)
+(add-hook 'kill-emacs-hook 'org-mobile-push)
+
+(setq org-default-notes-file (concat org-directory "/captured.org"))
+     (define-key global-map "\C-cc" 'org-capture)
+
+(setq org-capture-templates
+      '(("t" "Todo (any)" entry (file+headline "~/Dropbox/todo/todo.org" "Captured")
+             "* TODO %?\n  %i\n  %a")
+        ("w" "Workflow todo" entry (file+headline "~/Dropbox/todo/todo.org" "Workflow")
+             "** WORKFLOW %?\n  %i\n  %a")
+        ("c" "Chore todo" entry (file+headline "~/Dropbox/todo/todo.org" "Chore")
+             "** CHORE %?\n  %i\n  %a")
+        ("o" "Open Source idea" entry (file+headline "~/Dropbox/todo/todo.org" "Open Source")
+             "** OPENSOURCE %?\n  %i\n  %a")
+        ("r" "Read/Research" entry (file+headline "~/Dropbox/todo/todo.org" "Read")
+             "** READ %?\n  %i\n  %a")
+        ("u" "Urbint" entry (file+headline "~/Dropbox/todo/todo.org" "Urbint")
+             "** URBINT %?\n  %i\n  %a")
+      )
+)
+
+(setq org-log-done 'time)
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -87,64 +145,6 @@
 ;; line wrap
 (setq-default word-wrap t)
 ;; (toggle-truncate-lines 1)
-
-(setq org-todo-keywords
-       '((sequence "TODO"
-                   "WORKFLOW" "FIX" "FANCY" "RESEARCH"
-                   "IDEA" "MOVE" "INFO" "BLOG" "POST" "JACK"
-                   "OPENSOURCE"
-                   "STORYX"
-                   "PROTOTYPE"
-                   "TRIAGE"
-                   "|"               ;; <------- more likely to be typed
-                   "TODOLOL"         ;; at the center
-                   "INACTIVE"
-                   "REMINDER"
-                   "SCHEDULED"
-                   "BLOCKED"         ;; closer to 0 == quicker scrolling
-                   "DONE"            ;; <------- closer for Shift-<left> wrapping
-        ))
-)
-
-(setq org-src-fontify-natively t)
-
-(defun my/tangle-dotfiles ()
-  "If the current file is in '~/.dotfiles', the code blocks are tangled"
-  (when (equal (file-name-directory (directory-file-name buffer-file-name))
-               (concat (getenv "HOME") "/dotfiles/emacs.d/"))
-    (org-babel-tangle)
-    (message "%s tangled" buffer-file-name)))
-
-(add-hook 'after-save-hook #'my/tangle-dotfiles)
-
-(setq org-directory "~/Dropbox/todo/")
-(setq org-mobile-inbox-for-pull "~/Dropbox/todo/inbox.org")
-(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
-(setq org-mobile-files '("~/Dropbox/todo"))
-
-;;(add-hook 'after-init-hook 'org-mobile-pull)
-(add-hook 'kill-emacs-hook 'org-mobile-push)
-
-(setq org-default-notes-file (concat org-directory "/captured.org"))
-     (define-key global-map "\C-cc" 'org-capture)
-
-(setq org-capture-templates
-      '(("t" "Todo (any)" entry (file+headline "~/Dropbox/todo/todo.org" "Captured")
-             "* TODO %?\n  %i\n  %a")
-        ("w" "Workflow todo" entry (file+headline "~/Dropbox/todo/todo.org" "Workflow")
-             "** WORKFLOW %?\n  %i\n  %a")
-        ("c" "Chore todo" entry (file+headline "~/Dropbox/todo/todo.org" "Chore")
-             "** CHORE %?\n  %i\n  %a")
-        ("o" "Open Source idea" entry (file+headline "~/Dropbox/todo/todo.org" "Open Source")
-             "** OPENSOURCE %?\n  %i\n  %a")
-        ("r" "Read/Research" entry (file+headline "~/Dropbox/todo/todo.org" "Read")
-             "** READ %?\n  %i\n  %a")
-        ("u" "Urbint" entry (file+headline "~/Dropbox/todo/todo.org" "Urbint")
-             "** URBINT %?\n  %i\n  %a")
-      )
-)
-
-(setq org-log-done 'time)
 
 (use-package evil
   :commands (evil-mode local-evil-mode)
