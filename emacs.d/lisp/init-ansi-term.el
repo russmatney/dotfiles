c6bff1777616b51f624dd9405eb6b63bf29ed1af
Russell Matney <russell.matney@gmail.com>
2017-05-28 10:39:30 -0400

    fix(emacs:term): expose window movement bindings in term mode

--- emacs.d/lisp/init-ansi-term.el
+++ emacs.d/lisp/init-ansi-term.el
@@ -1,7 +1,13 @@
 ;;; init-extra.el --- Misc config without a home
 ;;; Commentary:
-;;; much thanks to: http://echosa.github.io/blog/2012/06/06/improving-ansi-term/
+;;;   much thanks to:
+;;;     - http://echosa.github.io/blog/2012/06/06/improving-ansi-term/
+;;;     - https://emacs.stackexchange.com/questions/328/how-to-override-keybindings-for-term
 ;;; Code:
+
+(use-package exec-path-from-shell
+  :config
+  (exec-path-from-shell-initialize))
 
 (defadvice term-sentinel (around my-advice-term-sentinel (proc msg))
   (if (memq (process-status proc) '(signal exit))
@@ -33,6 +39,16 @@
 (add-hook 'term-mode-hook 'my-term-hook)
 
 
+;; prevent term-mode from consuming the passed binding
+(defun expose-global-binding-in-term (binding)
+   (define-key term-raw-map binding
+     (lookup-key (current-global-map) binding)))
+
+;; expose for Ctrl-{h,j,k,l} window movement
+(expose-global-binding-in-term (kbd "C-l"))
+(expose-global-binding-in-term (kbd "C-h"))
+(expose-global-binding-in-term (kbd "C-j"))
+(expose-global-binding-in-term (kbd "C-k"))
 
 
 (provide 'init-extra)
