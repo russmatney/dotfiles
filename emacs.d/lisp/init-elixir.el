4e3a86bdc62d38f3eec496fdd95ddafcc396aeeb
Russell Matney <russell.matney@gmail.com>
2017-06-09 00:35:51 -0400

    fix(emacs): alchemist test toggle

--- emacs.d/lisp/init-elixir.el
+++ emacs.d/lisp/init-elixir.el
@@ -30,25 +30,8 @@
   "Toggle between a file and its tests in the current window."
   (interactive)
   (if (alchemist-utils-test-file-p)
-      (rm/alchemist-project-open-file-for-current-tests 'find-file)
+      (alchemist-project-open-file-for-current-tests 'find-file)
     (rm/alchemist-project-open-tests-for-current-file 'find-file)))
-
-(defun rm/alchemist-project-file-under-test (file directory)
-  "Return the file which are tested by FILE.
-DIRECTORY is the place where the file under test is located."
-  (let* ((filename (file-relative-name file (alchemist-project-root)))
-         (filename (replace-regexp-in-string "^test" directory filename))
-         (filename (replace-regexp-in-string "^apps/\\(.*\\)/test" "apps/\\1/test/" filename))
-         (filename (replace-regexp-in-string "_test\.exs$" "\.ex" filename)))
-    (concat (alchemist-project-root) filename)))
-
-(defun rm/alchemist-project-open-file-for-current-tests (opener)
-  "Visit the implementation file for the current buffer with OPENER."
-  (let* ((filename (alchemist-project-file-under-test (buffer-file-name) "web"))
-         (filename (if (file-exists-p filename)
-                       filename
-                     (alchemist-project-file-under-test (buffer-file-name) "lib"))))
-    (funcall opener filename)))
 
 (defun rm/alchemist-project-open-tests-for-current-file (opener)
   "Visit the test file for the current buffer with OPENER."
