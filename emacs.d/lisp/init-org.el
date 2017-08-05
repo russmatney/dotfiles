440f76938281a2275336741bd10f6830b2f23d82
Russell Matney <russell.matney@gmail.com>
2017-08-05 18:16:05 -0400

    feat(emacs): gtd org settings

--- emacs.d/lisp/init-org.el
+++ emacs.d/lisp/init-org.el
@@ -4,46 +4,53 @@
 
 (setq org-src-fontify-natively t)
 
-;;(org-babel-do-load-languages
-;;  'org-babel-load-languages
-;;  '((lisp . t))
-;;)
-
-;; (defun my/tangle-dotfiles ()
-;;   "If the current file is in '~/.dotfiles', the code blocks are tangled"
-;;   (when (equal (file-name-directory (directory-file-name buffer-file-name))
-;;                (concat (getenv "HOME") "/dotfiles/emacs.d/"))
-;;     (org-babel-tangle)
-;;     (message "%s tangled" buffer-file-name)))
-
-;; (add-hook 'after-save-hook #'my/tangle-dotfiles)
-
 (setq org-directory "~/Dropbox/todo/")
-(setq org-mobile-inbox-for-pull "~/Dropbox/todo/inbox.org")
-(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
-(setq org-mobile-files '("~/Dropbox/todo"))
-
-;;(add-hook 'after-init-hook 'org-mobile-pull)
-;;(add-hook 'kill-emacs-hook 'org-mobile-push)
 
 (setq org-default-notes-file (concat org-directory "/captured.org"))
      (define-key global-map "\C-cc" 'org-capture)
 
-(setq org-capture-templates
-      '(("t" "Todo (any)" entry (file+headline "~/Dropbox/todo/todo.org" "Captured")
-             "* TODO %?\n  Captured: %U\n  %a")
-        ("r" "Read/Research" entry (file+headline "~/Dropbox/todo/todo.org" "Read")
-             "* READ %?\n  Captured: %U\n  %a")
-      )
-)
+(setq org-capture-templates '(("t" "Todo [inbox]" entry
+                               (file+headline "~/Dropbox/todo/inbox.org" "Tasks")
+                               "* TODO %i%?")
+                              ("T" "Tickler" entry
+                               (file+headline "~/Dropbox/todo/tickler.org" "Tickler")
+                               "* %i%? \n %U")))
 
-(setq org-log-done 'time)
+(setq org-agenda-files '("~/Dropbox/todo/inbox.org"
+                         "~/Dropbox/todo/gtd.org"
+                         "~/Dropbox/todo/tickler.org"))
 
-(setq org-agenda-files
-      '("todo.org" "done.org" "read.org"))
+(setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
 
-(setq org-refile-use-outline-path 'file)
+;; (setq org-refile-use-outline-path 'file)
+(setq org-refile-targets '(("~/Dropbox/todo/gtd.org" :maxlevel . 3)
+                           ("~/Dropbox/todo/someday.org" :level . 1)
+                           ("~/Dropbox/todo/tickler.org" :maxlevel . 2)))
 
+;; (setq org-agenda-custom-commands
+;;       '(("o" "At the office" tags-todo "@office"
+;;          ((org-agenda-overriding-header "Office")))))
+
+(setq org-agenda-custom-commands
+      '(("o" "At the office" tags-todo "@office"
+         ((org-agenda-overriding-header "Office")
+          (org-agenda-skip-function #'my-org-agenda-skip-all-siblings-but-first)))))
+
+(defun my-org-agenda-skip-all-siblings-but-first ()
+  "Skip all but the first non-done entry."
+  (let (should-skip-entry)
+    (unless (org-current-is-todo)
+      (setq should-skip-entry t))
+    (save-excursion
+      (while (and (not should-skip-entry) (org-goto-sibling t))
+        (when (org-current-is-todo)
+          (setq should-skip-entry t))))
+    (when should-skip-entry
+      (or (outline-next-heading)
+          (goto-char (point-max))))))
+
+(defun org-current-is-todo ()
+  (string= "TODO" (org-get-todo-state)))
 
 ;; bindings to fix in org-mode
 (defun rm/org-mode-hook ()
