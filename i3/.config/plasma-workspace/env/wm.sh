3f7e499c41bf70b22aa063e5da975c93f458fcba
Russell Matney <russell.matney@gmail.com>
2018-11-27 19:17:02 -0500

    feat: i3 using alacritty, some other startup tweaks

--- i3/.config/plasma-workspace/env/wm.sh
+++ i3/.config/plasma-workspace/env/wm.sh
@@ -1,9 +1,2 @@
 # Disable KWin and use i3gaps as WM
 export KDEWM=/usr/bin/i3
-
-# Compositor (Animations, Shadows, Transparency)
-# xcompmgr -C
-~/.nix-profile/bin/compton -cCFb --backend glx --vsync opengl
-
-# start dropbox
-~/.dropbox-dist/dropboxd &
