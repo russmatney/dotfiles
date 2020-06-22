9a393779b1d7afce291d6a1830d2943682aab830
Russell Matney <russell.matney@gmail.com>
2020-06-22 16:04:39 -0400

    misc: cider dir-locals cruft

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -130,6 +130,9 @@
  '(safe-local-variable-values
    (quote
     ((cider-known-endpoints
+      ("yodo-clj-server" "localhost" "7888")
+      ("yodo-cljs-shadow" "localhost" "4201"))
+     (cider-known-endpoints
       ("yodo-cljs-shadow" "localhost" "4201"))
      (checkdoc-package-keywords-flag)
      (eval progn
@@ -165,4 +168,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:background "#191a2a")))))
