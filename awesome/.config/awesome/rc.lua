70206f9e2c0332020230ebfb6945bb3bd44eb85c
Russell Matney <russell.matney@gmail.com>
2020-07-25 14:25:34 -0400

    feat: name tags (workspaces)

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -215,7 +215,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6" }, s, awful.layout.layouts[1])
+    awful.tag({ "slack", "spotify", "web", "journal-notes", "awesome", "yodo" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
