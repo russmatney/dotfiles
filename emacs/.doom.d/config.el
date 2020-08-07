0a79540d4f811aa7217cf83088e6bcd52918d452
Russell Matney <russell.matney@gmail.com>
2020-08-06 23:57:17 -0400

    feat: dir-locals fn helpers

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -169,3 +169,34 @@
           (run-with-idle-timer
            aggressive-indent-sit-for-time
            nil #'aggressive-indent--indent-if-changed (current-buffer)))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; dir-locals
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; from https://emacs.stackexchange.com/questions/13080/reloading-directory-local-variables
+
+(defun my-reload-dir-locals-for-current-buffer ()
+  "reload dir locals for the current buffer"
+  (interactive)
+  (let ((enable-local-variables :all))
+    (hack-dir-local-variables-non-file-buffer)))
+
+(defun my-reload-dir-locals-for-all-buffer-in-this-directory ()
+  "For every buffer with the same `default-directory` as the
+current buffer's, reload dir-locals."
+  (interactive)
+  (let ((dir default-directory))
+    (dolist (buffer (buffer-list))
+      (with-current-buffer buffer
+        (when (equal default-directory dir))
+        (my-reload-dir-locals-for-current-buffer)))))
+
+;; (add-hook 'emacs-lisp-mode-hook
+;;           (defun enable-autoreload-for-dir-locals ()
+;;             (when (and (buffer-file-name)
+;;                        (equal dir-locals-file
+;;                               (file-name-nondirectory (buffer-file-name))))
+;;               (add-hook (make-variable-buffer-local 'after-save-hook)
+;;                         'my-reload-dir-locals-for-all-buffer-in-this-directory))))
