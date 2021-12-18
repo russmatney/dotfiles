37af4054c1a2384414eca74ca818c99653cfadfd
Russell Matney <russell.matney@gmail.com>
2021-12-18 11:24:10 -0500

    nvim: conjure, aniseed, jackin
    :ConjureSchool is awesome!

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,5 +1,9 @@
 vim.cmd [[packadd packer.nvim]]
 
+-- To reload: luafile lua/plugins.lua
+-- then: :PackerSync
+-- TODO write command and binding for this
+
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
@@ -8,11 +12,22 @@ return require('packer').startup(function(use)
   use_rocks {'luaformatter', server = 'https://luarocks.org/dev'}
   use_rocks {'penlight', 'lua-cjson', 'lua-resty-http', 'lpeg'}
 
+  -- Misc Repl support
+  use 'Olical/conjure'
+
+  -- Fennel
+  use 'Olical/aniseed'
+
+  -- Clojure
+  use 'clojure-vim/vim-jack-in'
+
   -- Local
   -- use '~/russmatney/some-nvim-plugin/blah.nvim'
 
-  -- Vim lifecycle
+  -- NeoVim infra
   use 'famiu/nvim-reload'
+  use 'tpope/vim-dispatch'
+  use 'radenling/vim-dispatch-neovim'
 
   -- Misc Syntax Plugins
   use 'vim-scripts/SyntaxComplete'
@@ -26,14 +41,24 @@ return require('packer').startup(function(use)
   use 'rschmukler/pangloss-vim-indent'
   use 'junegunn/goyo.vim'
   use 'junegunn/limelight.vim'
-  use 'othree/yajs.vim'
-  use 'othree/javascript-libraries-syntax.vim'
-  use 'othree/es.next.syntax.vim'
-  use 'Quramy/vim-js-pretty-template'
+  use 'vim-airline/vim-airline'
+  use 'vim-airline/vim-airline-themes'
+
+  -- Keybindings
   use {
     "folke/which-key.nvim",
     config = function() require("which-key").setup {} end
   }
+
+  -- Finders
+  use 'kien/ctrlp.vim'
+  use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/plenary.nvim'}}}
+  use 'scrooloose/nerdtree'
+  use 'ton/vim-bufsurf'
+  use 'yegappan/mru'
+  use 'tweekmonster/fzf-filemru'
+  use '~/.fzf'
+  use 'junegunn/fzf.vim'
 
   -- Alignment
   use 'godlygeek/tabular'
@@ -49,18 +74,7 @@ return require('packer').startup(function(use)
 
   -- Navigation
   use 'christoomey/vim-tmux-navigator'
-  use 'kien/ctrlp.vim'
-  use 'vim-airline/vim-airline'
-  use 'vim-airline/vim-airline-themes'
-  use 'scrooloose/nerdtree'
-  use 'ton/vim-bufsurf'
-  use 'yegappan/mru'
-  use 'tweekmonster/fzf-filemru'
-  use '~/.fzf'
-  use 'junegunn/fzf.vim'
-
   use 'vimoutliner/vimoutliner'
-
   use 'vim-scripts/BufOnly.vim'
 
   -- Color
