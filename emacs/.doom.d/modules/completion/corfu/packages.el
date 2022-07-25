782e19a4679341ef1bb91b6680ef5749d1cabe1e
Russell Matney <russell.matney@gmail.com>
2022-07-25 08:55:04 -0400

    fix: drop these pins b/c they're... gone?

--- emacs/.doom.d/modules/completion/corfu/packages.el
+++ emacs/.doom.d/modules/completion/corfu/packages.el
@@ -2,10 +2,10 @@
 ;;; completion/corfu/packages.el
 
 
-(package! corfu :pin "e77f89ca457295519891e1c5f365b0da25b3134e")
+(package! corfu)
 (when (featurep! +orderless)
-  (package! orderless :pin "87ab7e47e343285f7afd42779c78551332b8fd84"))
-(package! kind-icon :pin "f10bf6170769a1b0ef7d06347d1631fb6e71446b")
+  (package! orderless))
+(package! kind-icon)
 
 (package! cape :recipe (:host github :repo "minad/cape" :branch "main"))
 (package! corfu-doc :recipe (:host github :repo "galeo/corfu-doc" :branch "main"))
