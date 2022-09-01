ebbf580fa540fa38ad54110ef864cace110a1255
Russell Matney <russell.matney@gmail.com>
2022-09-01 15:04:03 -0400

    fix: remove dead defs bookmark

--- emacs/.doom.d/+hydra.el
+++ emacs/.doom.d/+hydra.el
@@ -144,9 +144,7 @@ Commands that stick around (this hydra supports multiple presses).
 
   ("B" (find-file "~/russmatney/clawe/src/clawe/defs/bindings.clj")
    "defs/bindings.clj" :column "clawe")
-  ("W" (find-file "~/russmatney/clawe/src/clawe/defs/workspaces.clj")
-   "defs/workspaces.clj")
-  ("l" (find-file "~/russmatney/clawe/resources/clawe.edn")
+  ("W" (find-file "~/russmatney/clawe/resources/clawe.edn")
    "clawe.edn")
 
   ("r" russ/open-project-file "...project file (readme?)" :column "Workspace....")
