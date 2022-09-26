d106670f03ddcd0d9858ec31e3e118ecd8ee14b8
Russell Matney <russell.matney@gmail.com>
2022-09-26 11:33:53 -0400

    emacs: keychain refresh helper
    Could add this dep as well, but this is fine for now:
    https://github.com/tarsius/keychain-environment/blob/master/keychain-environment.el

--- emacs/.doom.d/autoload/russ.el
+++ emacs/.doom.d/autoload/russ.el
@@ -222,3 +222,27 @@
 (defun russ/fix-visual-select ()
   (interactive)
   (setq-local transient-mark-mode t))
+
+
+
+
+;; https://github.com/tarsius/keychain-environment/blob/master/keychain-environment.el
+
+;;;###autoload
+(defun keychain-refresh-environment ()
+  "Set ssh-agent and gpg-agent environment variables.
+Set the environment variables `SSH_AUTH_SOCK', `SSH_AGENT_PID'
+and `GPG_AGENT' in Emacs' `process-environment' according to
+information retrieved from files created by the keychain script."
+  (interactive)
+  (let* ((ssh (shell-command-to-string "keychain -q --noask --agents ssh --eval"))
+         (gpg (shell-command-to-string "keychain -q --noask --agents gpg --eval")))
+    (list (and ssh
+               (string-match "SSH_AUTH_SOCK[=\s]\\([^\s;\n]*\\)" ssh)
+               (setenv       "SSH_AUTH_SOCK" (match-string 1 ssh)))
+          (and ssh
+               (string-match "SSH_AGENT_PID[=\s]\\([0-9]*\\)?" ssh)
+               (setenv       "SSH_AGENT_PID" (match-string 1 ssh)))
+          (and gpg
+               (string-match "GPG_AGENT_INFO[=\s]\\([^\s;\n]*\\)" gpg)
+               (setenv       "GPG_AGENT_INFO" (match-string 1 gpg))))))
