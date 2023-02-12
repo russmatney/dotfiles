4c30ad0e120d34c839bf61d2369184c9b7371df0
Russell Matney <russell.matney@gmail.com>
2023-02-12 10:22:38 -0500

    fix: bump vertico, magit, compat (fix corfu)
    Bumps a bunch of packages according to: https://github.com/doomemacs/doomemacs/pull/7075/files
    
    Requires manual fixes to modules/completion/vertico/config.el (also
    according to the PR).
    
    Fixes corfu without breaking vertico and magit.

--- emacs/.doom.d/packages.el
+++ emacs/.doom.d/packages.el
@@ -5,6 +5,35 @@
 ;; (package! compat :pin "e59e311d1ffc422f91e4e9c494598e978f5c2125"
 ;;           :recipe (:host github :repo "emacs-compat/compat"))
 
+;; https://github.com/doomemacs/doomemacs/pull/7075/files
+(package! vertico
+  :recipe (:host github :repo "minad/vertico"
+           :files ("*.el" "extensions/*.el"))
+  :pin "f303790546edecc67aa3bd5e23c68f982f1345dd")
+
+(package! orderless :pin "ae849b3d9f8c8a777e05816321ed2b00e8304447")
+(package! consult :pin "b22a7de62ee4adf766be2f867dee8b6980902bba")
+(package! compat :pin "2bedcb5ea91914e75d4905bc53e537b33f8f51e9")
+(package! consult-dir :pin "ed8f0874d26f10f5c5b181ab9f2cf4107df8a0eb")
+(when (modulep! :checkers syntax)
+  (package! consult-flycheck :pin "51b1b48e8dad314f9c9d963376f2ea8de94b97f2"))
+(package! embark :pin "4882b395cef98a517d530ffe483aa0dc7201158c")
+(package! embark-consult :pin "4882b395cef98a517d530ffe483aa0dc7201158c")
+
+(package! marginalia :pin "6d48ed54be87969e3ce53a24dbc63ec72ec6a91a")
+(package! wgrep :pin "edf768732a56840db6879706b64c5773c316d619")
+
+(when (modulep! +childframe)
+  (package! vertico-posframe
+    :recipe (:host github :repo "tumashu/vertico-posframe")
+    :pin "790f74b49d5309dc2f0e6a438e2e89007d591d07"))
+
+(package! git-commit :pin "6d325d90ba1374d48c4c7088f96864b678155f48")
+(package! consult-lsp :pin "f8db3252c0daa41225ba4ed1c0d178b281cd3e90")
+(package! magit :pin "6d325d90ba1374d48c4c7088f96864b678155f48")
+
+
+
 ;; lisp
 (package! dash)
 
