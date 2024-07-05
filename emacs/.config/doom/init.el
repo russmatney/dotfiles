e908b3d1b497b3651fe1951a6f590c8dc3c5a9db
Russell Matney <russell.matney@gmail.com>
2024-07-05 15:09:08 -0400

    chore: picom exclude, drop dead doom :tools rgb

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -176,7 +176,7 @@
        ;;pass
        ;;pdf             ; pdf enhancements
        ;;prodigy         ; FIXME managing external services & code builders
-       rgb               ; creating color strings
+       ;; rgb               ; creating color strings
        ;; taskrunner        ; taskrunner for all your projects
        ;;terraform       ; infrastructure as code
        tmux              ; an API for interacting with tmux
