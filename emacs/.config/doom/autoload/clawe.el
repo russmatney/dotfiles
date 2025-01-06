dc8e346ef0f97181a7f619938fa4e14d4d531c54
Russell Matney <russell.matney@gmail.com>
2025-01-06 12:37:04 -0500

    emacs: toggle clawe/ingest-on-save on/off via interactive func

--- emacs/.config/doom/autoload/clawe.el
+++ emacs/.config/doom/autoload/clawe.el
@@ -4,19 +4,31 @@
   "Comment out one or more s-expressions."
   nil)
 
+(setq clawe/ingest-on-save t)
+
+;;;###autoload
+(defun clawe/toggle-ingest-on-save ()
+  (interactive)
+  (let ((new-val (not clawe/ingest-on-save)))
+    (print! "setting clawe ingest-on-save: %s" new-val)
+    (setq clawe/ingest-on-save new-val)))
+
 ;;;###autoload
 (defun clawe/doctor-ingest-this-file ()
   (interactive)
-  (unless (and buffer-file-name (file-exists-p buffer-file-name))
-    (user-error "Buffer is not visiting any file"))
-  (let ((path (buffer-file-name (buffer-base-buffer))))
-    (message (concat "doctor ingesting file " path))
-    ;; consider 'without-popups' or naming and ignoring a common clawebb buffer
-    (async-shell-command
-     (concat "clawebb -x clawe.doctor/ingest-file --path " path)
-     ;; TODO add random int/timestamp to this buffer name
-     ;; TODO add filename to command
-     (concat "*clawebb-" (file-name-base (buffer-file-name)) "*"))))
+  (if (not clawe/ingest-on-save)
+      (message "ingest on save disabled, nothing doing")
+
+    (unless (and buffer-file-name (file-exists-p buffer-file-name))
+      (user-error "Buffer is not visiting any file"))
+    (let ((path (buffer-file-name (buffer-base-buffer))))
+      (message (concat "doctor ingesting file " path))
+      ;; consider 'without-popups' or naming and ignoring a common clawebb buffer
+      (async-shell-command
+       (concat "clawebb -x clawe.doctor/ingest-file --path " path)
+       ;; TODO add random int/timestamp to this buffer name
+       ;; TODO add filename to command
+       (concat "*clawebb-" (file-name-base (buffer-file-name)) "*")))))
 
 (comment
  (message "hi")
