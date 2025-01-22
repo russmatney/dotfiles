bb093cc69e23d4ec22709cc5a4c59d09beba55de
Russell Matney <russell.matney@gmail.com>
2025-01-22 18:13:51 -0500

    nvim: restore last session for dir when no args

--- nvim/.config/nvim/lua/plugins/astrocore.lua
+++ nvim/.config/nvim/lua/plugins/astrocore.lua
@@ -69,5 +69,23 @@ return {
         ["<Leader><spc>"] = { "<C-^>", desc = "Toggle Last File" },
       },
     },
+    autocmds = {
+      -- disable alpha autostart
+      alpha_autostart = false,
+      restore_session = {
+        {
+          event = "VimEnter",
+          desc = "Restore previous directory session if neovim opened with no arguments",
+          nested = true, -- trigger other autocommands as buffers open
+          callback = function()
+            -- Only load the session if nvim was started with no args
+            if vim.fn.argc(-1) == 0 then
+              -- try to load a directory session using the current working directory
+              require("resession").load(vim.fn.getcwd(), { dir = "dirsession", silence_errors = true })
+            end
+          end,
+        },
+      },
+    },
   },
 }
--- nvim/.config/nvim/lua/plugins/astrolsp.lua
+++ nvim/.config/nvim/lua/plugins/astrolsp.lua
@@ -97,7 +97,7 @@ return {
     },
     -- A custom `on_attach` function to be run after the default `on_attach` function
     -- takes two parameters `client` and `bufnr`  (`:h lspconfig-setup`)
-    on_attach = function(client, bufnr)
+    on_attach = function(_client, _bufnr)
       -- this would disable semanticTokensProvider for all clients
       -- client.server_capabilities.semanticTokensProvider = nil
     end,
