f15a026cc6628f24ea84b0a958ff443500dc0ede
Russell Matney <russell.matney@gmail.com>
2025-01-22 14:39:52 -0500

    nvim: copy in current astrovim setup

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,37 @@
+-- Customize Mason plugins
+
+---@type LazySpec
+return {
+  -- use mason-lspconfig to configure LSP installations
+  {
+    "williamboman/mason-lspconfig.nvim",
+    -- overrides `require("mason-lspconfig").setup(...)`
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        -- add more arguments for adding more language servers
+      },
+    },
+  },
+  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
+    "jay-babu/mason-null-ls.nvim",
+    -- overrides `require("mason-null-ls").setup(...)`
+    opts = {
+      ensure_installed = {
+        "stylua",
+        -- add more arguments for adding more null-ls sources
+      },
+    },
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    -- overrides `require("mason-nvim-dap").setup(...)`
+    opts = {
+      ensure_installed = {
+        "python",
+        -- add more arguments for adding more debuggers
+      },
+    },
+  },
+}
