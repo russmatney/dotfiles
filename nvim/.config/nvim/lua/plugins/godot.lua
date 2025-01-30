e7e2071a53dd0cd0c4b09b5641fdbdfe8bd258bf
Russell Matney <russell.matney@gmail.com>
2025-01-30 10:51:38 -0500

    nvim: basic godot config, ignore lazy-lock.json

---
+++ nvim/.config/nvim/lua/plugins/godot.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = { filetypes = { extension = { gdshaderinc = "gdshaderinc" } } },
+  },
+  {
+    "QuickGD/quickgd.nvim",
+    ft = { "gdshader", "gdshaderinc" },
+    cmd = { "GodotRun", "GodotRunLast", "GodotStart" },
+    opts = {
+      godot_path = "/bin/godot",
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed =
+          require("astrocore").list_insert_unique(opts.ensure_installed, { "gdscript", "glsl", "godot_resource" })
+      end
+    end,
+  },
+}
