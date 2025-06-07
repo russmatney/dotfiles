207200056f46df1aaef0f2a836f2b8a8a3ead09b
Russell Matney <russell.matney@gmail.com>
2025-06-07 11:46:24 -0400

    emacs: faster default model, limit copilot-mode

--- emacs/.config/doom/+ai.el
+++ emacs/.config/doom/+ai.el
@@ -6,16 +6,19 @@
   :config
   (setq aidermacs-use-architect-mode t)
   ;; (setq aidermacs-default-model "ollama_chat/llama3.1-claude:latest")
-  (setq aidermacs-default-model "openrouter/deepseek/deepseek-r1:free")
+  (setq aidermacs-default-model "ollama_chat/gemma3:1b")
+  ;; (setq aidermacs-default-model "openrouter/deepseek/deepseek-r1:free")
 
   ;; Optional: Set specific model for architect reasoning
-  (setq aidermacs-architect-model "ollama_chat/devstral:24b")
+  ;; (setq aidermacs-architect-model "ollama_chat/devstral:24b")
 
   ;; Optional: Set specific model for code generation
-  (setq aidermacs-editor-model "ollama_chat/llama3.1-claude:latest"))
+  ;; (setq aidermacs-editor-model "ollama_chat/llama3.1-claude:latest")
+  )
 
 (use-package! copilot
-  :hook (prog-mode . copilot-mode)
+  ;; consider more modes, but don't opt into everything
+  :hook (gdscript-mode . copilot-mode)
   :bind (:map copilot-completion-map
               ("C-TAB" . 'copilot-accept-completion)
               ("C-<tab>" . 'copilot-accept-completion))
