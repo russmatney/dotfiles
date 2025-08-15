4185586b27437fc0f8607260d5cc1f9a61d3e0a8
Russell Matney <russell.matney@gmail.com>
2025-08-15 15:27:41 -0400

    fix: update polybar kill

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+pkill polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
