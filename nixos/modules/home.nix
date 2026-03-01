ee2692477b3086787b4509ff0bcfa970fbb640c3
Russell Matney <russell.matney@gmail.com>
2026-03-01 15:05:56 -0500

    nix: drop unused var

--- nixos/modules/home.nix
+++ nixos/modules/home.nix
@@ -1,8 +1,4 @@
-{ pkgs, config, ... }:
-
-let
-  sym = config.lib.file.mkOutOfStoreSymlink;
-in {
+{ ... }: {
   programs.home-manager.enable = true;
 
   wayland.windowManager.hyprland.systemd.enable = false;
